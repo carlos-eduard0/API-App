bc87ae0bff38444175cd89f3ebbd5cffc0487014
carlos-eduard0 <eduard.mascarenhas@hotmail.com>
2020-04-01 09:42:10 -0400

    update

--- src/controllers/EmpresaController.js
+++ src/controllers/EmpresaController.js
@@ -10,27 +10,27 @@ module.exports = {
         return res.json(empresa);
     },
 
-    async create(req, res){
+    async create(req, res) {
         const { email, nome_empresa, senha, confirmar_senha } = req.body;
 
         const id = crypto.randomBytes(4).toString('HEX');
 
         const servicoss = ["teste1", "teste2", "teste"];
 
-        const servicosArray = servicoss ;
+        const servicosArray = servicoss;
 
-        if(senha != confirmar_senha){
-            return res.json ({ mensagem: 'As senhas não estão corretas'});
+        if (senha != confirmar_senha) {
+            return res.json({ mensagem: 'As senhas não estão corretas' });
         }
 
-            await connection('empresa').insert({
-                id,
-                email,
-                nome_empresa,
-                senha,
-                servicos: {servicosArray},
-            });
-            return res.json({ id, confirmar_senha, servicosArray });
+        await connection('empresa').insert({
+            id,
+            email,
+            nome_empresa,
+            senha,
+            servicos: { servicosArray },
+        });
+        return res.json({ id, confirmar_senha, servicosArray });
     },
 
 }
