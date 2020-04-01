038779daa050a0e9ade476d1104eaa0ad644b82e
carlos-eduard0 <eduard.mascarenhas@hotmail.com>
2020-04-01 14:03:24 -0400

    add try catch, tratar erros

--- src/controllers/EmpresaController.js
+++ src/controllers/EmpresaController.js
@@ -10,22 +10,25 @@ module.exports = {
         return res.json(empresa);
     },
 
-    async create(req, res){
-        const { email, nome_empresa, senha, confirmar_senha } = req.body;
+    async create(req, res) {
+        try {
+            const { nome_empresa, email, senha, confirmar_senha } = req.body;
 
-        const id = crypto.randomBytes(4).toString('HEX');
+            const id = crypto.randomBytes(4).toString('HEX');
 
-        if(senha != confirmar_senha){
-            return res.status(400).json({ error: 'As senhas não correspodem'});
-        }
+            if (senha != confirmar_senha) {
+                return res.send({ message: 'senhas não conferem' });
+            }
 
             await connection('empresa').insert({
                 id,
-                email,
                 nome_empresa,
+                email,
                 senha,
             });
-            return res.json({ id, confirmar_senha });
-    },
-
+            return res.status(200).send({ message: "cadastrado", id })
+        } catch (error) {
+            return res.status(400).send({ error: 'alguma coisa errada' });
+        }
+    }
 }
