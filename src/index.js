e5387d622bd60c15785cc8d3c980214b30a45e92
carlos-eduard0 <eduard.mascarenhas@hotmail.com>
2020-04-02 15:57:59 -0400

    update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ const rotas = require('./routes');
 
 const app = express();
 
-app.use(cors({origin:'https://engine-frontend.herokuapp.com/'}));
+app.use(cors({
+    origin: "https://engine-frontend.herokuapp.com/",
+    methods: GET,HEAD,PUT,PATCH,POST,DELETE,
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  }));
 app.use(express.json());
 app.use(rotas);
 
