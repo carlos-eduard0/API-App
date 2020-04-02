3ca35a6ccb613fbbf89ff3a5984f3065a1010fd7
carlos-eduard0 <eduard.mascarenhas@hotmail.com>
2020-04-02 10:58:01 -0400

    update

--- knexfile.js
+++ knexfile.js
@@ -2,53 +2,23 @@ require("dotenv").config();
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'pg',
-    version: '12.2',
-    connection: {
-      DATABASE_URL: process.env.DATABASE_URL,
-      host : process.env.BD_HOST, 
-      user : process.env.BD_USER,  
-      password : process.env.BD_PASS, 
-      database : process.env.BD_DATA, 
+      client: 'pg',
+      connection: {
+          host: '127.0.0.1',
+          user: process.env.TODO_DB_USER,
+          password: process.env.TODO_DB_PW,
+          database: 'todos_test'
+      },
+      migrations: {
+          directory: './src/database/migrations',
+      },
   },
-    migrations: {
-      directory: "./src/database/migrations", ///BD_DIR
-    },
-    useNullAsDefault: true,
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      client: 'pg',
+      connection: process.env.DATABASE_URL,
+      migrations: {
+        directory: './src/database/migrations',
+      },
+  },
 };
