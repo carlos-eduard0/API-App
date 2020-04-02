13e9b29d0b29914053c7dc00c640058a09b5311e
carlos-eduard0 <eduard.mascarenhas@hotmail.com>
2020-04-02 14:07:54 -0400

    update

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,6 @@
-const knex = require('knex');
-const configuration = require('../../knexfile');
 
-const connection = knex(configuration.production);
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../../knexfile')[environment];
+const connection = require('knex')(configuration);  
 
 module.exports = connection;
