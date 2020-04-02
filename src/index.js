1a37e8cbdb759db5cb423269ecdde6dbdabf2d03
carlos-eduard0 <eduard.mascarenhas@hotmail.com>
2020-04-02 15:30:17 -0400

    update cors

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const rotas = require('./routes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin:process.env.FRONT_END_URL || 3000}));
 app.use(express.json());
 app.use(rotas);
 
