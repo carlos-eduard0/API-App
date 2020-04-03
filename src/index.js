d7b8b3b3a917263fec14807084364c388a994030
carlos-eduard0 <eduard.mascarenhas@hotmail.com>
2020-04-02 23:24:06 -0400

    update cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
+const cors = require('cors');
 const rotas = require('./routes');
 
 
 const app = express();
 
-
+app.use(cors());
 app.use(express.json());
 app.use(rotas);
 
--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,14 @@
 // router, express;
 const express =  require('express');
-const cors = require('cors');
+
 const rotas = express.Router();
 
 const EmpresasController = require('./controllers/EmpresaController');
 const SessionController = require('./controllers/SessionConttroller');
 
 
-const corsOptions = {
-    origin: 'https://www.engine-app.com/',
-  }
-
-rotas.post('/empresa', cors(corsOptions), EmpresasController.create);
-rotas.get('/empresa', cors(corsOptions), EmpresasController.index);
+rotas.post('/empresa', EmpresasController.create);
+rotas.get('/empresa', EmpresasController.index);
 
 rotas.post('/sessions', SessionController.login);
 
