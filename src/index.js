649ce11687a1d184c063d16efb88fc473c9a56bb
carlos-eduard0 <eduard.mascarenhas@hotmail.com>
2020-04-02 23:21:31 -0400

    cors

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 const express = require('express');
-const cors = require('cors');
+
 const rotas = require('./routes');
 
 
 const app = express();
 
-app.use(cors({
-    origin: 'https://www.engine-app.com/',
-    methods: "POST"
-}));
+
 app.use(express.json());
 app.use(rotas);
 
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,18 @@
 // router, express;
 const express =  require('express');
-
+const cors = require('cors');
 const rotas = express.Router();
 
 const EmpresasController = require('./controllers/EmpresaController');
 const SessionController = require('./controllers/SessionConttroller');
 
 
-rotas.post('/empresa', EmpresasController.create);
-rotas.get('/empresa', EmpresasController.index);
+const corsOptions = {
+    origin: 'https://www.engine-app.com/',
+  }
+
+rotas.post('/empresa', cors(corsOptions), EmpresasController.create);
+rotas.get('/empresa', cors(corsOptions), EmpresasController.index);
 
 rotas.post('/sessions', SessionController.login);
 
