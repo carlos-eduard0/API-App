b6b2b1e4415a5ac3cf6db660f49155f0b2fbe493
carlos-eduard0 <eduard.mascarenhas@hotmail.com>
2020-04-06 11:18:06 -0400

    update

--- src/controllers/EmpresaController.js
+++ src/controllers/EmpresaController.js
@@ -13,7 +13,7 @@ module.exports = {
 
     async create(req, res) {
         try {
-            const { nome, senha, confirmar_senha, nome_empresa, email, telefone, cpf, cnpj, rg, orgao_emissor, cep, cidade, uf, bairro, endereco, numero, complemento} = req.body;
+            const { nome, senha, confirmar_senha, nome_empresa, email, telefone, cpf, cnpj, rg, orgao_emissor, cep, cidade, uf, bairro, endereco, numero, complemento, nome_banco, agencia, conta, digito } = req.body;
 
             const id = crypto.randomBytes(4).toString('HEX');
 
@@ -43,9 +43,12 @@ module.exports = {
                 numero,
                 complemento,
                 senha:hash,
+                nome_banco,
+                agencia,
+                conta,
+                digito,
+                id_empresa: id,
             });
-
-         
             return res.status(200).send({ message: "cadastrado", id });
         } catch (error) {
             return res.status(400).send({ error: 'alguma coisa errada' });
