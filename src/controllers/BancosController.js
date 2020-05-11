5644a5614575deeabeed6eaec02da8c0b5eb12fa
Ronan70 <42750619+Ronan70@users.noreply.github.com>
2020-05-11 20:00:31 -0300

    arquivo desnecessário

--- src/controllers/BancosController.js
+++
@@ -1,9 +0,0 @@
-
-const connection = require('../database/connection');
-
-module.exports = {
-    async index(req, res) {
-        const bancos = await connection('bancos').select('*');
-        return res.json(bancos)
-    },
-};
