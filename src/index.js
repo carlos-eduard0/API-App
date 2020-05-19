060a0445513a7ead8ceac7103a5b48cfbf03dfbb
carlos-eduard0 <eduard.mascarenhas.ce@gmail.com>
2020-05-19 18:23:34 -0400

    up cors

--- src/index.js
+++ src/index.js
@@ -5,18 +5,10 @@ const rotas = require('./routes');
 
 const app = express();
 
-
+app.use(cors());
 app.use(express.json());
-
 app.use(rotas);
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    app.use(cors());
-    next();
-});
 
 
 app.listen(process.env.PORT || 7070);
