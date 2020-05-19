a88cedaa25ca3df4877f85aa1c804b54ba8adcb2
carlos-eduard0 <eduard.mascarenhas.ce@gmail.com>
2020-05-19 17:57:17 -0400

    up cors

--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ const rotas = require('./routes');
 
 const app = express();
 
-app.use(cors());
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "https://engine-frontend.herokuapp.com/");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 app.use(express.json());
 app.use(rotas);
 
