f08e1df48e11d68a25ecb3a80fbbd6dbcf576eb0
carlos-eduard0 <eduard.mascarenhas.ce@gmail.com>
2020-05-30 16:45:48 -0400

    up

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ const rotas = require('./routes');
 const app = express();
 
 app.use(cors({
-    "origin": "*",
+    "origin": "*", 
+    "Access-Control-Allow-Origin":"*",
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "preflightContinue": false,
-    "optionsSuccessStatus": 204
+    "optionsSuccessStatus": 204,
+    "Access-Control-Allow-Methods": 'GET,HEAD,PUT,PATCH,POST,DELETE',
 }));
 app.use(express.json());
 app.use(rotas);
