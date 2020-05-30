9e9fa53055cd9bd221498a90898b716dba25df14
carlos-eduard0 <eduard.mascarenhas.ce@gmail.com>
2020-05-30 16:34:04 -0400

    up

--- src/index.js
+++ src/index.js
@@ -5,15 +5,16 @@ const rotas = require('./routes');
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", ["https://engine-company.com/login"] );
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
-    next();
-});
-app.use(express.json());
-app.use(rotas);
+var corsOptions = {
+    origin: 'https://engine-company.com/',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
+app.use(express.json());
+
+app.use(rotas, cors(corsOptions), function (req, res, next) {
+    res.json({ msg: 'This is CORS-enabled for only example.com.' })
+})
 
 
 app.listen(process.env.PORT || 7070);
