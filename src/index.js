44b2c7e6b6515136d7515aea4d28e68b251af04d
carlos-eduard0 <eduard.mascarenhas.ce@gmail.com>
2020-05-30 16:41:53 -0400

    up

--- src/index.js
+++ src/index.js
@@ -5,18 +5,15 @@ const rotas = require('./routes');
 
 const app = express();
 
-var corsOptions = {
-    origin: 'https://engine-company.com/',
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-    AccessControlAllowMethods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-}
-
+app.use(cors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+}));
 app.use(express.json());
+app.use(rotas);
 
-app.use(rotas, cors(corsOptions), function (req, res, next) {
-    res.json({ msg: 'This is CORS-enabled for only example.com.' })
-})
 
 
 app.listen(process.env.PORT || 7070);
