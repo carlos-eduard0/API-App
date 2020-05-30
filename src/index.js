4c9b23095703af2f3588ad1ab018538e059a4bdb
carlos-eduard0 <eduard.mascarenhas.ce@gmail.com>
2020-05-30 16:39:54 -0400

    up

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express();
 var corsOptions = {
     origin: 'https://engine-company.com/',
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    AccessControlAllowMethods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
 }
 
