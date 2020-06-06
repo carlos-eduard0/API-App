7cd23bb400483b1692623657e17ce4991e38980f
carlos-eduard0 <eduard.mascarenhas.ce@gmail.com>
2020-06-05 21:47:20 -0400

    up

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const path = require("path");
 const rotas = require('./routes');
 
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With ,content-type ,Authorization, RefreshToken');
-    res.setHeader('Access-Control-Expose-Headers', 'RefreshToken,token');
-    next();
-})
+app.use(cors());
+app.options('*', cors());
 app.use(express.json());
-
-app.use('/file', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')));
-
 app.use(rotas);
 
 
