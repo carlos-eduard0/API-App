7ed7b316f70ec32b1edea4d697061be5a4f0f876
EKaynan <erickaynan11@gmail.com>
2020-06-12 20:21:49 -0300

    arrumando a mudança de senha

--- src/controllers/EmailController.js
+++ src/controllers/EmailController.js
@@ -53,29 +53,32 @@ module.exports = {
 	// },
 
 	async forgot(req, res){
-		const {email} = req.body
-        const token = crypto.randomBytes(20).toString('HEX');
-        const date = new Date();
-		const today = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+(date.getDate()+1);
+		// const {email} = req.body
+  //       const token = crypto.randomBytes(20).toString('HEX');
+  //       const date = new Date();
+		// const today = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+(date.getDate()+1);
 
-        const emailData = {
-        	to: email,
-		    subject: "Mudança de senha",
-		    html: "<h1>https://engine-company.com/codigo/senha/"+token+"</h1>"
-        };
+  //       const emailData = {
+  //       	to: email,
+		//     subject: "Mudança de senha",
+		//     html: "<h1>https://engine-company.com/codigo/senha/"+token+"</h1>"
+  //       };
 
-        const att = await connection('empresas')
-        .where('email', email)
-        .update({
-		    updateCode: token,
-		    updateCode_expires: today
-	  	});      	
+  //       const att = await connection('empresas')
+  //       .where('email', email)
+  //       .update({
+		//     updateCode: token,
+		//     updateCode_expires: today
+	 //  	});      	
 
-        if(!att){
-        	return res.json({message:'erro'});
-        } else {
-        	sendEmail.enviar(emailData);
-        	return res.status(200).json({message:'email enviado'});
-        }
+  //       if(!att){
+  //       	return res.json({message:'erro'});
+  //       } else {
+  //       	sendEmail.enviar(emailData);
+        	// return res.status(200).json({message:'email enviado'});
+  //       }
+  
+  	return res.status(200).json({message:'email enviado'});
+
 	}
 }
