90a620c0446b6cbe49de40cbcfc1556a1ff9637b
EKaynan <erickaynan11@gmail.com>
2020-06-12 19:49:20 -0300

    arrumando a mudança de senha

--- knexfile.js
+++ knexfile.js
@@ -2,34 +2,34 @@ require("dotenv").config();
 // Update with your config settings.
 
 module.exports = {
-  // development: {
-  //     client: 'pg',
-  //     connection: {
-  //         host: '127.0.0.1',
-  //         user: process.env.DB_USER,
-  //         password: process.env.DB_PW,
-  //         database: process.env.DB_DATABASE,
-  //     },
-  //     migrations: {
-  //         directory:'./src/database/migrations',
-  //     },
-  // },
+  development: {
+      client: 'pg',
+      connection: {
+          host: '127.0.0.1',
+          user: process.env.DB_USER,
+          password: process.env.DB_PW,
+          database: process.env.DB_DATABASE,
+      },
+      migrations: {
+          directory:'./src/database/migrations',
+      },
+  },
 
   //Banco do Eric 
-  development: {
-    client: 'mysql',
-    connection: {
-      filename: './src/database/db.mysql',
-      host:'localhost',
-      user:'root',
-      password:'',
-      database : 'Engine'
-    },
-    migrations: {
-      directory: './src/database/migrations'
-    },
-    useNullAsDefault: true,
-  },
+  // development: {
+  //   client: 'mysql',
+  //   connection: {
+  //     filename: './src/database/db.mysql',
+  //     host:'localhost',
+  //     user:'root',
+  //     password:'',
+  //     database : 'Engine'
+  //   },
+  //   migrations: {
+  //     directory: './src/database/migrations'
+  //   },
+  //   useNullAsDefault: true,
+  // },
 
   production: {
       client: 'pg',
