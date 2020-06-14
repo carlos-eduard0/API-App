c719c5e63f377e0fed36dc99f247cf41d247de5c
EKaynan <erickaynan11@gmail.com>
2020-06-14 05:01:26 -0300

    arrumando trocar senha

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,5 +1,5 @@
 
-const environment = process.env.NODE_ENV || 'development';
+const environment = process.env.NODE_ENV || 'production';
 const configuration = require('../../knexfile')[environment];
 const connection = require('knex')(configuration);  
 
